35fead4eabacd727d5d5a08c30fe80b24f81cd1f
ponnagani123 <user37@trainux01.jrf11vejvqfuhbwnm0ewggipoa.ax.internal.cloudapp.net>
2024-09-03 06:16:36 +0000

    updated

---
+++ arithmetic.c
@@ -0,0 +1,12 @@
+/* simple arithmetic operation 
+on two integers*/
+#include<stdio.h>
+int main()
+{
+	printf("enter any two numbers");
+	int a,b;
+	scanf("%d%d",&a,&b);
+	printf("sum of %d and %d is %d",a,b,a+b);
+	printf("- of %d and %d is %d",a,b,a-b);
+	return 0;
+}
